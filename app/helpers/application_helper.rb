eb5862affa1daedb33ce7f430eedb5d0bdae6f2d
Mahad Kalam <mahadkalam1@proton.me>
2025-07-15 14:23:23 +0100

    Shop UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def admin_tool(class_name = "", element = "div", **options, &block)
+    return unless authenticated?
+
+    content = capture(&block)
+
+    content_tag(element,
+                content,
+                class: "#{"p-2" unless element == "span"} border-2 border-dashed border-orange-500 bg-orange-500/10 w-fit h-fit #{class_name}",
+                **options)
+  end
 end
